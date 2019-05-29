fae84082f7bf20a994c5838a2f45da58db642c61
Modjo AI <r.meuter@hotmail.com>
2019-05-29 22:25:29 +0200

    Problème héritage

--- user/models.py
+++ user/models.py
@@ -5,28 +5,29 @@ from django.contrib.auth.models import User
 
 
 # Create your models here.
-
+"""
 class timeStampeModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     ## Permettre de rendre une class abstraite donc ne peut pas etre hériter !
     class Meta:
         abstract = True
+"""
 
         
 
-class User(timeStampeModel):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+class user(User):
     nom = models.CharField(max_length=200)
     
     def __str__(self):
-        return "Etudiant ".format(self.user.username)
-
-class Patient(timeStampeModel):
+        return "Etudiant {0}".format(self.user.username)
+"""
+class Patient(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     
     
 class Journal(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
-    Patient  = models.ForeignKey('Patient', on_delete=models.CASCADE)
+    Patient  = models.ForeignKey(Patient, on_delete=models.CASCADE)
     
+"""
