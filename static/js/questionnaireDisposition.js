efd101d1771fb37a0f127ca4e3d295ec46481be6
Modjo AI <r.meuter@hotmail.com>
2019-07-18 20:13:00 +0200

    ecriture de la fonction appelSelection dans questionnaireDisposition

--- static/js/questionnaireDisposition.js
+++ static/js/questionnaireDisposition.js
@@ -4,17 +4,31 @@ $(function(){
     });
 });
 
-choixPrincipal=[["Texte et image",appelForm() ] , ["Question", appelSelection()] , ["Réponse libre", appelSelection()]];
-choixQuestion=["Réponse à choix unique", "Réponse à choix multiple", "Réponse libre"];
+var choixPrincipal=[
+    ["Texte et image",appelForm()], 
+    ["Question", appelSelection("Type de reponse à la question",listInput)] , 
+    ["vidéo", appelForm()],
+];
 
-listInput={
-    "listInputOnePossiblity":["Bouton Radio", "Selection"],
-    "listInputManyPossiblity":["Case à cocher", "Selection multiple"],
-    "listInputFree":["Reponse gradué", "Texte libre"],
-};
+var listInput=[
+    ["Bouton Radio", appelForm()],
+    ["Selection", appelForm()],
+    ["Case à cocher", appelForm()],
+    ["Selection multiple", appelForm()],
+    ["Reponse graduée", appelForm()],
+    ["Texte libre", appelForm()]
+              ];
 
-function appelSelection(){
-    
+
+function appelSelection(choix, listechoix){
+    srt='<div class="input-group mb-3"><div class="input-group-prepend"><label class="input-group-text" for="inputGroupSelect01">Type de question</label></div><select class="custom-select" id="inputGroupSelect01">';
+    let smallint=0
+    for quest in listechoix:
+        srt+=('<option value="'+smallint+'">'+quest[0] +'</option>');
+        smallint++;
+    srt+='</select></div>';
+    $('#SelectionPrincipal').change(function(){        $('#ZoneForm').html();
+    });
 }
 
 function appelForm(){
