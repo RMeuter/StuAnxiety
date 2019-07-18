3cc317ccbb5fee16728501661c182a0c2bc099f1
Modjo AI <r.meuter@hotmail.com>
2019-07-18 18:16:29 +0200

    Mise en place de liste et fonction vide js pour disposition des activité d'une section

--- static/js/questionnaireDisposition.js
+++ static/js/questionnaireDisposition.js
@@ -1,3 +1,22 @@
 $(function(){
+    $('#SelectionPrincipal').change(function(){
+        $('#ZoneForm').html(choixPrincipal[int($('#SelectionPrincipal').val())][1]);
+    });
+});
+
+choixPrincipal=[["Texte et image",appelForm() ] , ["Question", appelSelection()] , ["Réponse libre", appelSelection()]];
+choixQuestion=["Réponse à choix unique", "Réponse à choix multiple", "Réponse libre"];
+
+listInput={
+    "listInputOnePossiblity":["Bouton Radio", "Selection"],
+    "listInputManyPossiblity":["Case à cocher", "Selection multiple"],
+    "listInputFree":["Reponse gradué", "Texte libre"],
+};
+
+function appelSelection(){
     
-})
+}
+
+function appelForm(){
+    
+}
