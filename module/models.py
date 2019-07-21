4b45ba6535d4596bf128c5f1c6fcead651cdbfda
Modjo AI <r.meuter@hotmail.com>
2019-07-21 21:20:45 +0200

    Modification du model module, unification questionnaire et module, simplification des types section et question

--- module/models.py
+++ module/models.py
@@ -3,32 +3,21 @@ from django.core.validators import MaxValueValidator
 from ckeditor_uploader.fields import RichTextUploadingField
 
 
-#-------------------------------------- Questionnaire------------------------------------------------------------
-class Questionnaire(models.Model):
-    """
-    Le nom sera le nom
-    """
-    nom = models.CharField(max_length=150)
-    nbQuestion=models.PositiveSmallIntegerField(default=0)
-    isVisible=models.BooleanField(default=False)
-    isJournal=models.BooleanField(default=False)
-    def __str__(self):
-        return self.nom
+#-------------------------------------- Question------------------------------------------------------------
     
 class Question(models.Model):
    
-    INPUT_TYPE = [(1, 'Selectionner une réponse'), (2, 'Bouton radio'),(3, 'Case à cocher'),(4, "Reponse libre")]
-    inputType = models.IntegerField(choices=INPUT_TYPE)
-    MULTIPLE_INPUT = [(True, 'Plusieurs choix de réponses possibles'), (False, 'Une seul réponse')]
-    isMultipleRep = models.BooleanField(default=False, choices=MULTIPLE_INPUT)
+    question =   models.CharField(max_length=250)
     consigne =   models.CharField(max_length=250, null=True, blank=True)
     intervaleGraduerInput = models.PositiveSmallIntegerField(default=0, validators=[MaxValueValidator(100)]) # si différent de 0 c'est un input de type gradué
     
-    questionnaire  = models.ForeignKey(Questionnaire, on_delete=models.CASCADE,blank=True, null=True)
-    question =   models.CharField(max_length=250)
-    ordre = models.PositiveSmallIntegerField(blank=True, null=True)
+    INPUT_TYPE = [(1, 'Selectionner une réponse ou plusieurs réponses'), (2, 'Bouton radio'),(3, 'Case à cocher (réponse multiple)'),(4, "Reponse texte libre"),(5, "Reponse gradué libre")]
+    inputType = models.IntegerField(choices=INPUT_TYPE)
+    MULTIPLE_INPUT = [(True, 'Plusieurs choix de réponses possibles'), (False, 'Une seul réponse')]
+    isMultipleRep = models.BooleanField(default=False, choices=MULTIPLE_INPUT)
+    
     def __str__(self):
-        return "{0} {1}".format(self.question,self.ordre)
+        return "{0} {1}".format(self.question)
         
         
 class Reponse(models.Model):
@@ -48,7 +37,9 @@ class Module (models.Model):
     #Pour opitimiser les requet et savoir si le module peut etre visible
     nbSection=models.PositiveSmallIntegerField(default=0)
     isVisible=models.BooleanField(default=False)
-    questionnaireDependant = models.ForeignKey(Questionnaire, on_delete=models.SET_NULL,null=True, blank=True) 
+    isQuestionnaireOnly=models.BooleanField(default=False)
+    isJournal=models.BooleanField(default=False)
+    questionnaireDependant = models.ForeignKey(Module, on_delete=models.SET_NULL,null=True, blank=True) 
     def __str__(self):
         return self.nom
 
@@ -57,8 +48,12 @@ class Section(models.Model):
     """
     Il y a trois choix possible mais pas en meme temps, soit une vidéo, soit un pdf, soit une question
     """
+    ####################### Base de la section
     titre = models.CharField(max_length=250)
     ordre = models.PositiveSmallIntegerField()
+    SECTION_TYPE = [(1, 'Texte et images'), (2, 'Question'),(3, 'Vidéo')]
+    SectionType = models.IntegerField(choices=INPUT_TYPE)
+    ####################### Intégration de la section
     question = models.ForeignKey('Question', on_delete=models.SET_NULL,null=True, blank=True)
     module = models.ForeignKey('Module', on_delete=models.CASCADE)
     text = RichTextUploadingField(null=True, blank=True)
@@ -68,6 +63,7 @@ class Section(models.Model):
     def __str__(self):
         return "{0} : {1} {2}".format(self.module, self.titre,self.ordre )
 
+#-------------------------------------- Sequence ------------------------------------------------------------
 
 class Sequence(models.Model):
     """
@@ -76,17 +72,7 @@ class Sequence(models.Model):
     nom= models.CharField(max_length=250)
     possede = models.ManyToManyField(Module, through="ordre", blank=True)
     nbModule=models.PositiveSmallIntegerField(default=0)
-    
     def __str__(self):
-        """
-        try :
-            listModule = Sequence.possede.filter(sequence=self.pk)
-            seq = "" +Sequence.nom +" dont le parcours est "
-            for module in listModule :
-                seq += listModule.nom + ", "
-            return seq
-        except NameError:
-        """
         return self.nom
     
     class Meta:
