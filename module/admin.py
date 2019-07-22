0de9be7f196166f5f6fb2be4ed41a423c6d77a4e
Modjo AI <r.meuter@hotmail.com>
2019-07-22 19:25:48 +0200

    Suppression du models questionnaires et responsables"

--- module/admin.py
+++ module/admin.py
@@ -1,20 +1,13 @@
 from django.contrib import admin
-from .models import Module, Section, Questionnaire, Question, Reponse, Sequence, Ordre
+from .models import Module, Section, Question, Reponse, Sequence, Ordre
 
 # Register your models here.
 """
 """
 admin.site.register(Reponse)
 admin.site.register(Question)
-admin.site.register(Questionnaire)
 admin.site.register(Module)
 admin.site.register(Section)
 admin.site.register(Sequence)
 admin.site.register(Ordre)
 
-class ModuleAdmin(admin.ModelAdmin):
-    list_display   = ('nom', 'updated_at', 'created_at', )
-    list_filter    = ('auteur','categorie',)
-    date_hierarchy = 'created_at'
-    ordering       = ('created_at', )
-    search_fields  = ('nom')
--- user/admin.py
+++ user/admin.py
@@ -1,24 +1,8 @@
 from django.contrib import admin
-from .models import Responsable, Clinicien, Patient, Population, variableEtude, Dossier, Agenda, Parcours, enAttente, Message, Resultat
-# Register your models here.
+from .models import Clinicien, Patient, Population, variableEtude, Dossier, Agenda, Parcours, enAttente, Message, Resultat
 
 
-class ResponsableAdmin(admin.ModelAdmin):
-    list_display   = ('username')
-    list_filter    = ('hopital')
-    date_hierarchy = 'user.date_joined'
-    ordering       = ('user.date_joined', )
-    search_fields  = ('hopital')
-
-class ClinicienAdmin(admin.ModelAdmin):
-    list_display   = ('username')
-    list_filter    = ('hopital', 'Responsable')
-    date_hierarchy = 'user.date_joined'
-    ordering       = ('user.date_joined', )
-    search_fields  = ('hopital', 'Responsable')
-
-admin.site.register(Responsable)#,ResponsableAdmin )
-admin.site.register(Clinicien)#,ClinicienAdmin )
+admin.site.register(Clinicien)
 admin.site.register(Patient)
 admin.site.register(Population)
 admin.site.register(variableEtude)
