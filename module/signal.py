40d3a298daa3353b07452443681e7af7c906fe71
Modjo AI <r.meuter@hotmail.com>
2019-08-21 16:49:00 +0200

    Ajout d'automatise d'ordre dans les sections

--- module/signal.py
+++ module/signal.py
@@ -1,6 +1,7 @@
-from django.db.models.signals import pre_delete, post_save
+from django.db.models.signals import pre_delete, post_save, pre_save
 from django.dispatch import receiver
 from .models import Ordre, Section, Question, Module
+from django.db.models import Max
 
 
 @receiver(pre_delete, sender=Section)
@@ -20,7 +21,7 @@ def delete_ordre_pre_delete_section(sender, instance, *args, **kwargs):
         mod.save()
 
 @receiver(post_save, sender=Section)
-def add_nb_section(sender, instance, created, *args, **kwargs):
+def add_nb_section_and_verify_typeSection(sender, instance, created, *args, **kwargs):
     if created:
         mod = Module.objects.get(pk=instance.module.pk)
         mod.nbSection+=1
@@ -37,5 +38,9 @@ def add_nb_section(sender, instance, created, *args, **kwargs):
                 instance.save()
             else:
                 instance.delete()
-        
+
+@receiver(pre_save, sender=Section)
+def verify_double(sender, instance, *args, **kwargs):
+        if Section.objects.filter(module=instance.module).exists():
+            instance.ordre = Section.objects.filter(module=instance.module).aggregate(Max('ordre')).ordre +1
                 
