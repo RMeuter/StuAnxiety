5cb3b37b23574d54caabfd401827762bc064e41f
Modjo AI <r.meuter@hotmail.com>
2019-08-25 19:11:07 +0200

    ajout de presence d'activité du clinicien ou du patient

--- user/consumers.py
+++ user/consumers.py
@@ -2,26 +2,20 @@ from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
 import json
 from .models import Message, Patient
-
+from django.utils import timezone
 
 class ChatPatientConsumer(WebsocketConsumer):
     def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['UUCIpatient']
         self.room_group_name = 'chat_%s' % self.room_name
         self.patient = Patient.objects.get(pk= self.room_name)
+
         # Join room group
         async_to_sync(self.channel_layer.group_add)(
             self.room_group_name,
             self.channel_name
         )
         self.accept()
-
-        query = Message.objects.filter(patient=self.patient)[:30]
-        for message in query:
-            self.send(text_data=json.dumps({
-                'isCli': query.isClinicien,
-                'message': query.message
-            }))
 
 
     def disconnect(self, close_code):
@@ -37,8 +31,19 @@ class ChatPatientConsumer(WebsocketConsumer):
         message = text_data_json['message']
         isCli = text_data_json['isCli']
 
+        # Intégration d'un nouveau message
         newMess = Message.objects.create(patient=self.patient, clinicien=self.patient.clinicienACharge, message=message, isClinicien=isCli)
         newMess.save()
+
+        # recuperaption des anciens messages
+        if isCli == None:
+            query = Message.objects.filter(patient=self.patient)[:30]
+            for message in query:
+                self.send(text_data=json.dumps({
+                    'isCli': query.isClinicien,
+                    'message': query.message
+                }))
+
         # Send message to room group
         async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
@@ -60,3 +65,4 @@ class ChatPatientConsumer(WebsocketConsumer):
             'message': message
         })
         )
+
