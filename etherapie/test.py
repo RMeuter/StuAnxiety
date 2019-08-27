9110540970dbb6857a9a34afad6731cf3cda6527
Modjo AI <r.meuter@hotmail.com>
2019-08-27 13:47:52 +0200

    Mise en place de test dans l'application de base

--- etherapie/test.py
+++ etherapie/test.py
@@ -1,1 +1,30 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import resolve, reverse
+from .urls import urlpatterns
+
+class TestUrls (TestCase):
+
+    def setUp(self):
+        self.client =Client()
+        self.home_url=reverse("home")
+        self.FAQ_url=reverse("FAQ")
+        self.contact_url=reverse("contact")
+
+
+    def test_home_GET(self):
+        reponse = self.client.get(self.home_url)
+
+        self.assertEquals(reponse.status_code, 200)
+        self.assertTemplateUsed(reponse, "home.html")
+
+    def test_FAQ_GET(self):
+        reponse = self.client.get(self.FAQ_url)
+
+        self.assertEquals(reponse.status_code, 200)
+        self.assertTemplateUsed(reponse, "FAQ.html")
+
+    def test_contact_GET(self):
+        reponse = self.client.get(self.contact_url)
+
+        self.assertEquals(reponse.status_code, 200)
+        self.assertTemplateUsed(reponse, "contact.html")
