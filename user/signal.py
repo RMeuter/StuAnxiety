bd8708d70bafff57667311b327b6a3ad8b37ab89
Modjo AI <r.meuter@hotmail.com>
2019-08-29 12:41:26 +0200

    Ajout de commentaire

--- user/signal.py
+++ user/signal.py
@@ -1,7 +1,7 @@
 #Model et attribut de signal !
-from django.db.models.signals import post_save, pre_save
+from django.db.models.signals import post_save
 from django.dispatch import receiver
-from .models import Patient, Clinicien, Population, enAttente
+from .models import Patient, Clinicien, Population
 from module.models import Sequence
 from django.contrib.auth.models import User, Group
 
@@ -9,25 +9,20 @@ from django.contrib.auth.models import User, Group
 from django.contrib.auth.models import Permission
 from django.contrib.contenttypes.models import ContentType
 
-# checker la permission d'une séquence
-from django.contrib.auth.decorators import permission_required
-"""
-S'entrainer : https://openclassrooms.com/fr/courses/1871271-developpez-votre-site-web-avec-le-framework-django/1873451-les-signaux-et-middlewares
-"""
-
 ########################################## Affectation auto sequence pour groupe 2 ##################################
 @receiver(post_save, sender=Patient)
 def Affecte_sequence(sender,created, instance, **kwargs):
+    """
+    La focntion affecte une séquence de module vide si le patient est en groupe de catégorie 2. S'il en possède une et qu'il
+    n'est pas en 2, elle est automatique détruite.
+    """
     print("fonction affect sequence")
     if instance.groupePatients != None :
         if instance.groupePatients.categorie == 2 and instance.sequence == None:
-            print("passe 1")
             seq = Sequence.objects.create(nom="Sequence de {0} {1}".format(instance.user.first_name,instance.user.last_name))
-            print(seq)
             instance.sequence = seq
             instance.save()
         elif instance.groupePatients.categorie != 2 and instance.sequence != None :
-            print("passe 2")
             instance.sequence.delete()
             
 ########################################## Affectation de permission ##################################
@@ -35,32 +30,23 @@ def Affecte_sequence(sender,created, instance, **kwargs):
 @receiver(post_save, sender=Population)
 def Affectation_Permission_Groupe(sender,created, instance, **kwargs):
     """
-    On permet les différent parcours selon les catégories de groupe
-    https://stackoverflow.com/questions/10131271/invalid-literal-error-when-adding-a-user-permission-to-a-django-user-object
-    https://www.vinta.com.br/blog/2016/controlling-access-a-django-permission-apps-comparison/
-    
-    https://cheat.readthedocs.io/en/latest/django/permissions.html
-    
-    Faire attention au répetion permission car elle sont écraser dans le else!
-    
-    ###########Attention à la mise en cache !##############
-    
+    Si le groupe ne vient pas d'etre crée on écrase ces permissions
+    Ensuite la fonction affecte les permissions ciblés selon sa catégorie
+
     """
     print("fonction affect groupe et pop")
     if not created :
         # Une fois qu'on modifie un groupe on enlève toute ces permissions et on le recré selon la categorie
         instance.groupe.permissions.clear()
-        
     content_type = ContentType.objects.get_for_model(Population)
+
     if instance.categorie == 1:
         permission = Permission.objects.get(content_type=content_type, codename='parcours_Clinicien')
         instance.groupe.permissions.add(permission)
-
     elif instance.categorie == 5:
         permission = Permission.objects.get(content_type=content_type, codename='parcours_Patient_Non_Suivie')
         instance.groupe.permissions.add(permission)
-
-    else :    
+    else :
         permission = Permission.objects.get(content_type=content_type, codename='parcours_Patient_Suivie')
         instance.groupe.permissions.add(permission)
 
@@ -72,17 +58,13 @@ def Affectation_Permission_Groupe(sender,created, instance, **kwargs):
             permission = Permission.objects.get(content_type=content_type, codename='Sequence_Groupal')
             instance.groupe.permissions.add(permission)
 
-"""
-@receiver(pre_save, sender=enAttente)
-def verifie_double_et_impossibilite_enAttente(sender, instance, *args, **kwargs):
-    if enAttente.objects.filter(patient=instance.patient, module=instance.module, forClinicien=False).exists():
-        enAttente.objects.filter(patient=instance.patient, module=instance.module, forClinicien=False).delete()
-"""
 ############################### Integration de groupe pour les permission#####################################
 
 @receiver(post_save, sender=Patient)
 def integre_group(sender,created, instance, *args, **kwargs):
-    
+    """
+     La fonction efface tous les liens avec les autre groupes pour affecter uniquement le lien appartenant au groupe affecter
+    """
     user=User.objects.get(pk=instance.user.pk)
     user.groups.clear()
     gp =Group.objects.get(pk=instance.groupePatients.groupe.pk)
@@ -90,7 +72,9 @@ def integre_group(sender,created, instance, *args, **kwargs):
 
 @receiver(post_save, sender=Clinicien)
 def integre_group(sender,created, instance, *args, **kwargs):
-    
+    """
+    La fonction efface tous les liens avec les autre groupes pour affecter uniquement le lien appartenant au groupe affecter
+    """
     user=User.objects.get(pk=instance.user.pk)
     user.groups.clear()
     gp =Group.objects.get(pk=instance.equipe.groupe.pk)
